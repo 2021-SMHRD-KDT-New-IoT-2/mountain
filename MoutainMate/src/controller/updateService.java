8fac52212aa65d5460208b1eaa32428150250e3a
smhrd <smhrd@210.223.239.165>
2021-10-27 11:41:37 +0900

    로그인기능, 회원가입, 중복체크, 회원수정 추가 -범종

---
+++ MoutainMate/src/controller/updateService.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.UserDAO;
+import model.UserVO;
+
+
+@WebServlet("/updateService")
+public class updateService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("euc-kr"); //요청데이터의 인코딩 방식 지정
+		
+		HttpSession session=request.getSession(); //세션 객체 생성
+		UserVO vo=(UserVO)session.getAttribute("member");//현재 로그인한 사용자의(수정전) 정보
+		String id = vo.getid(); //현재 로그인한 사용자의 이메일
+		
+		//수정에 사용할 정보
+		String pw=request.getParameter("pw");
+		String name=request.getParameter("name");
+		String phoneNumber=request.getParameter("phoneNumber");
+		String birth=request.getParameter("birth");
+		String gender=request.getParameter("gender");
+		String manager=request.getParameter("manager");
+		
+		//dao 기능 호출 -> 수정 여부를 판단 할 수 있는 값 반환
+		// 수정 성공일 경우에는 콘솔 -> "수정성공!"
+		// 수정 실패일 경우에는 콘솔 -> "수정 실패!"
+		
+		UserDAO dao = new UserDAO();
+		int cnt=dao.update(id,pw, name, phoneNumber, birth, gender, manager);
+		if(cnt>0) {
+			System.out.println("수정 성공");
+			
+			 UserVO vo2 =new UserVO(id, name, phoneNumber, birth, gender, manager);
+			session.setAttribute("User", vo2);
+			response.sendRedirect("MainPage.jsp");
+		}else {
+			System.out.println("수정실패");
+			response.sendRedirect("MainPage.jsp");
+		}
+}
+
+}
---
+++ MoutainMate/src/model/UserVO.java
@@ -0,0 +1,46 @@
+package model;
+
+public class UserVO {
+
+	private String id;
+	private String pw;
+	private String name;
+	private String phoneNumber;
+	private String birth;
+	private String gender;
+	private String manager;
+	
+	//user »ý¼ºÀÚ
+	public UserVO(String id, String name,
+			String phoneNumber, String birth, String gender,
+			String manager) {
+		
+	}
+	
+	public	String getid() {
+		return id;
+	}
+	
+	
+	public	String getname() {
+		return name;
+	}
+	
+	public	String getphoneNumber() {
+		return phoneNumber;
+	}
+	
+	public	String getbirth() {
+		return birth;
+	}
+	
+	public	String getgender() {
+		return gender;
+	}
+	
+	public	String getmanager() {
+		return manager;
+	}
+	
+	
+}
