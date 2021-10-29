61f924d536683c3e7da47b84043baf0138147521
smhrd <smhrd@220.71.97.138>
2021-10-29 09:31:27 +0900

    도훈-UserVO추가

--- MoutainMate/src/model/UserVO.java
+++ MoutainMate/src/model/UserVO.java
@@ -4,20 +4,34 @@ public class UserVO {
 
 	//필드(속성)
 		private String id;
-		
 		private String name;
-		
-		
-		
-		
+		private String phoneNumber;
+		private String birth;
+		private String gender;
+		private String mgr;
+		private String pw;
+
 		//생성자
 		public UserVO(String id, String name) {
 			
 			this.id=id;
-			this.name=name;
-			
-			
+			this.name=name;	
 		}
+		
+		public UserVO(String id, String name, String phoneNumber, String birth, String gender, String mgr, String pw) {
+			
+			this.id=id;
+			this.name=name;	
+			this.phoneNumber=phoneNumber;
+			this.birth=birth;	
+			this.gender=gender;
+			this.name=name;	
+			this.pw=pw;
+			this.mgr=mgr;
+		}
+	
+		
+		
 		//메서드(get)
 		public String getid() {
 			return id;
@@ -25,6 +39,20 @@ public class UserVO {
 		public String getname() {
 			return name;
 		}
-		
+		public String getphoneNumber() {
+			return phoneNumber;
+		}
+		public String getbirth() {
+			return birth;
+		}
+		public String getgender() {
+			return gender;
+		}
+		public String getpw() {
+			return pw;
+		}
+		public String getmgr() {
+			return mgr;
+		}
 		
 	}
