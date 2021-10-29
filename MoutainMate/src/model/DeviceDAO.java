66688c85fe8a7cb610d83337571a4a1c26f310ca
smhrd <smhrd@121.147.52.218>
2021-10-29 12:02:42 +0900

    우경-device vo,dao 생성

---
+++ MoutainMate/src/model/DeviceDAO.java
@@ -0,0 +1,56 @@
+package model;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class DeviceDAO {
+	Connection conn = null;
+	PreparedStatement psmt = null;
+	ResultSet rs = null;
+	UserVO vo =null;
+	int cnt = 0;	
+	ArrayList<UserVO> arr =null;
+
+	public void connection() { // db연결
+		try {
+			// 1. 드라이브 동적로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
+			// 해도쌤이 주신 db 링크 가져와주세요
+//			url: project-db-stu.ddns.net
+//			port: 1524
+//			user: campus_b_1_1025
+//			password: smhrd1
+			String user = "campus_b_1_1025";
+			String password = "smhrd1";
+
+			// 2. 데이터 연결 객체 (Connection)생성
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("연결실패");
+		}
+	}
+	
+
+	public void close() {
+		try {
+			if(rs!=null) {
+				rs.close();
+			}
+			if(rs!=null) {
+			psmt.close();
+			}
+			if(rs!=null) {
+			conn.close();
+			} 
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+	}
+	
+	
+}
---
+++ MoutainMate/src/model/DeviceVO.java
@@ -0,0 +1,50 @@
+package model;
+
+public class DeviceVO {
+	
+	String p_id;
+	String m_id;
+	String user_id;
+	String rental_date;
+	String r_start_time;
+	String r_return_time;
+	
+	
+	public String getP_id() {
+		return p_id;
+	}
+	public void setP_id(String p_id) {
+		this.p_id = p_id;
+	}
+	public String getM_id() {
+		return m_id;
+	}
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getRental_date() {
+		return rental_date;
+	}
+	public void setRental_date(String rental_date) {
+		this.rental_date = rental_date;
+	}
+	public String getR_start_time() {
+		return r_start_time;
+	}
+	public void setR_start_time(String r_start_time) {
+		this.r_start_time = r_start_time;
+	}
+	public String getR_return_time() {
+		return r_return_time;
+	}
+	public void setR_return_time(String r_return_time) {
+		this.r_return_time = r_return_time;
+	}
+	
+}
