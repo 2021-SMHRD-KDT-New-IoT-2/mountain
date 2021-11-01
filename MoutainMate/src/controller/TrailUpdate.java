de31d2ec03b2b914d192a4bc6f34f471519b3d59
smhrd <smhrd@220.71.97.138>
2021-11-01 10:49:53 +0900

    도훈-완주코스 서블렛

--- MoutainMate/src/controller/TrailUpdate.java
+++ MoutainMate/src/controller/TrailUpdate.java
@@ -7,12 +7,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.MountainVO;
+import model.UserDAO;
+
 @WebServlet("/TrailUpdate")
 public class TrailUpdate extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
 		
+		String id = request.getParameter("id");
+		String mroad = request.getParameter("mroad");
+		
+		
+		UserDAO dao = new UserDAO();
+		
+		int cnt = dao.userClearTime(id, mroad);
+		
+		if (cnt > 0) {
+			System.out.println("완주등산로 추가 성공");
+			response.sendRedirect("Trail.jsp");
+		} else {
+			System.out.println("완주등산로 추가 실패");
+			response.sendRedirect("Trail.jsp");
+		}
 	}
 
 }
